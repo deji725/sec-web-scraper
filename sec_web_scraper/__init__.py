8c68e96d2ed0c645567e1935601d041f57514d14
deji725 <deji725@gmail.com>
2023-03-02 17:15:26 -0500

    added testing suite from example template

---
+++ sec_web_scraper/__init__.py
@@ -0,0 +1,2 @@
+from sec_web_scraper._version import __version__
+from sec_web_scraper.Scraper import * 
---
+++ sec_web_scraper/tests/test_all.py
@@ -0,0 +1,12 @@
+from sec_web_scraper import Scraper,get_document_given_link
+from unittest.mock import patch
+
+test_link ="https://www.sec.gov/Archives/edgar/data/20/0000893220-96-000500.txt"
+def test_get_document_given_link():
+    assert get_document_given_link(test_link) is not None
+
+
+#   @patch('builtins.print')
+#   def test_print_hello(mock_print):
+#       print_hello()
+#       assert mock_print.call_args.args == ("Hello, world!",)
