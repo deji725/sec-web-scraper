edbcbaf520a3d732a280c3395d1d6ad22b9a1dd0
deji725 <deji725@gmail.com>
2023-03-04 03:26:27 -0500

    Fixed commmits, almost got scared

---
+++ sec_web_scraper/__main__.py
@@ -0,0 +1,25 @@
+from Scraper import get_company_filings_given_cik, iterate_over_filings
+
+
+def main():
+    sample_10k = "https://www.sec.gov/Archives/edgar/data/20/0000893220-96-000500.txt"
+    # create_selenium_browser_headless()
+    # build_index_sec(1993, 2000)
+    # raw_txt = get_document_given_link(sample_10k)
+    # doc_tags = get_document_tags(raw_txt)
+
+    # print("These are the doc tags ")
+    # print(doc_tags)
+    # Add checking to see if length of cik is 10 digits
+    apple_cik = get_company_filings_given_cik("0000320193")
+    print("These are the keys in the official SEC API based on CIK")
+    print(apple_cik.keys())
+
+    print("Iterating over the filings....")
+    n = iterate_over_filings(apple_cik["filings"])
+    print(f"This is the file count {n}")
+    print(sample_10k)
+
+
+if __name__ == "__main__":
+    main()
